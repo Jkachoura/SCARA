13eaa82c02b009eb0ed2aecef6f7f67be85b7de2
JKachoura <jaouadkachoura@hotmail.com>
2023-11-16 13:20:14 +0100

    inverse kinematics fixed and ball screw spline movement added

--- master/main.cpp
+++ master/main.cpp
@@ -1,17 +1,51 @@
-// main.cpp
+#include "master.h"
+#include "slave.h"
 #include "scara.h"
 #include <iostream>
 
-int main() {
-    // Create an instance of the SCARA class with specific arm lengths
+//-90000 is -90 graden
+int main(int argc, char* argv[]) {
+    char ifaceName[] = "\\Device\\NPF_{DEA85026-34BA-4C8B-9840-A3CE7793A348}";
+    Master ecMaster(ifaceName, 8000);
+
     SCARA scaraRobot(250, 280);
 
-    // Use the SCARA class to calculate joint angles
-    JointAngles angles = scaraRobot.calculateJointAngles(238, 380, false);
+    JointAngles angles = scaraRobot.calculateJointAngles(423.70, -45.51, false);
 
-    // Output the results
-    std::cout << "Joint 1 Angle: " << angles.j1 << " degrees\n";
-    std::cout << "Joint 2 Angle: " << angles.j2 << " degrees\n";
+    if (ecMaster.connected()) {
+        Slave ecSlave(ecMaster, 1);
+        Slave ecSlave2(ecMaster, 2);
+        Slave ecSlave3(ecMaster, 3);
 
-    return 0;
+        ecSlave.acknowledge_faults();
+        ecSlave.enable_powerstage();
+        ecSlave2.acknowledge_faults();
+        ecSlave2.enable_powerstage();
+        ecSlave3.acknowledge_faults();
+        ecSlave3.enable_powerstage();
+        
+        ecSlave3.position_task(0, 400, true);
+        ecSlave2.position_task(0 * 1000, 40000, true, false);
+        ecSlave.position_task(0 * 1000, 40000, true, false);
+
+        Sleep(1000);
+   
+        ecSlave2.position_task(angles.j2 * 1000, 40000, true, false);
+        ecSlave.position_task(angles.j1 * 1000, 40000, true, false);
+        ecSlave3.position_task(275 * 1000, 400, true);
+
+        Sleep(1000);
+
+        ecSlave3.position_task(0, 400, true);
+        ecSlave2.position_task(0 * 1000, 40000, true, false);
+        ecSlave.position_task(0 * 1000, 40000, true, false);
+
+        Sleep(1000);
+
+        return EXIT_SUCCESS;
+    }
+    else {
+        return EXIT_FAILURE;
+    }
 }
+
